229f2f07e76567a4ec272c5959a5e534893f7b02
smsaravanan0 <smsaravanan0@gmail.com>
2019-06-14 11:00:55 +0530

    added code

---
+++ src/org/jack/Forloop.java
@@ -0,0 +1,24 @@
+package org.jack;
+
+public class Forloop {
+	public static void main(String[] args) {
+		for(int i=0;i<=9;i++) {
+			for(int j=1;j<i;j++) {
+				System.out.print("i");
+			}
+			
+			
+			System.out.println();
+			}
+		
+	}
+
+
+		
+}
+
+	
+		
+	
+
+
